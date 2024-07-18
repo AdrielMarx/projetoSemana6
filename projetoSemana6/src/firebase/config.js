16d032ec5bc46494fcf423f12f822630fcf77d6d
Adriel Marx <155272647+AdrielMarx@users.noreply.github.com>
2024-07-18 14:08:05 -0300

    configurando config.js

--- projetoSemana6/src/firebase/config.js
+++ projetoSemana6/src/firebase/config.js
@@ -1,19 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDH46a9tnhHxnyTEGC1ERFbnLgqes0-bd4",
-  authDomain: "my-task-693ae.firebaseapp.com",
-  projectId: "my-task-693ae",
-  storageBucket: "my-task-693ae.appspot.com",
-  messagingSenderId: "1011717230776",
-  appId: "1:1011717230776:web:346d75145d7149f72a644d"
+  apiKey: "AIzaSyAO18eRnUbVy2lZ5QtmEjgdLU5JRBZbRSY",
+  authDomain: "gerenciador-contatos.firebaseapp.com",
+  projectId: "gerenciador-contatos",
+  storageBucket: "gerenciador-contatos.appspot.com",
+  messagingSenderId: "956038790529",
+  appId: "1:956038790529:web:32362e8aa8c0433ca2fc80"
 };
 
-// app => objeto com todas as configurações do firebase
 export const app = initializeApp(firebaseConfig);
-// auth => objeto com as configurações do authentication
 export const auth = getAuth(app)
-// db => objeto com as configurações do Firestore
 export const db = getFirestore(app)
